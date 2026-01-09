68e02c2631050d2ac199da0df59f40bbd990d05e
Hyeladi Malgwi <46991842+H3SAN@users.noreply.github.com>
2026-01-09 18:09:01 +0200

    verify admitad

--- app/root.tsx
+++ app/root.tsx
@@ -29,6 +29,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="verify-admitad" content="b6a5920d4f" />
         <Meta />
         <Links />
       </head>
